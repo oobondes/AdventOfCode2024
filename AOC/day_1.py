ab00573221c8ffcf7c03596f09a1b3b77fcb597a
Austin Vanasse <Austin.L.Vanasse.mil@army.mil>
2024-12-01 19:14:55 +0000

    completed day 1

--- AOC/day_1.py
+++ AOC/day_1.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 #solution for https://adventofcode.com/2024/day/1
 def part_1_day_1(text:str):
-    return None
+    nums = [int(num) for num in text.split()]
+    left = sorted(nums[::2])
+    right = sorted(nums[1::2])
+    return sum((abs(x-y) for x,y in zip(left,right)))
 
 def part_2_day_1(text:str):
-    return None
+    nums = [int(num) for num in text.split()]
+    left = sorted(nums[::2])
+    right = sorted(nums[1::2])
+    return sum((x * right.count(x) for x in left))
 
