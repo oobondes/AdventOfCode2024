4b6b8768bf118a868166caa6a32687ac32251cab
Austin Vanasse <Austin.L.Vanasse.mil@army.mil>
2024-12-04 06:21:39 -0500

    updates leaderboard printing so it prints in score order, then day order.

--- AOC/__main__.py
+++ AOC/__main__.py
@@ -6,21 +6,23 @@ from json import loads
 
 if len(argv) == 3 and argv[1].lower() in ("-g", "--generate-file"):
     from pathlib import Path
+
     print("generating template files now")
     year = int(argv[2])
     aoc_dir = Path("AOC")
-    for day in range(1,25):
-        file = aoc_dir/f"day_{day}.py"
-        file.write_text(f"#! /usr/bin/env python3\n"
-                        f"#solution for https://adventofcode.com/{year}/day/{day}"
-                        f"\n"
-                        f"def part_1_day_{day}(text:str):\n"
-                        f"    return None\n"
-                        f"\n"
-                        f"def part_2_day_{day}(text:str):\n"
-                        f"    return None\n"
-                        f"\n"
-                        )
+    for day in range(1, 25):
+        file = aoc_dir / f"day_{day}.py"
+        file.write_text(
+            f"#! /usr/bin/env python3\n"
+            f"#solution for https://adventofcode.com/{year}/day/{day}"
+            f"\n"
+            f"def part_1_day_{day}(text:str):\n"
+            f"    return None\n"
+            f"\n"
+            f"def part_2_day_{day}(text:str):\n"
+            f"    return None\n"
+            f"\n"
+        )
 
     print("files generated")
 elif len(argv) == 4 and argv[1].lower() in ("--board", "-b"):
@@ -29,12 +31,12 @@ elif len(argv) == 4 and argv[1].lower() in ("--board", "-b"):
     group_id = int(argv[3])
     data = aoc.get_leaderboards(group_id)
     data = loads(data)
-    for member, member_data in sorted(data["members"].items(), key=lambda x:x[0]):
+    for member, member_data in sorted( data["members"].items(), key=lambda x: (x[1].get("local_score"), x[0]), reverse=True,):
         name = member_data.get("name", member) or member
-        print(f"{name}")
-        for day, day_data in sorted(member_data["completion_day_level"].items(), key=lambda x:x[0]):
-            part_1_time = datetime.fromtimestamp(int(day_data.get("1",{}).get("get_star_ts", 0xFFFFFFFF)))
-            part_2_time = day_data.get("2",{}).get("get_star_ts")
+        print(f"{name} ({member_data.get('local_score')})")
+        for day, day_data in sorted(member_data["completion_day_level"].items(), key=lambda x: x[0]):
+            part_1_time = datetime.fromtimestamp(int(day_data.get("1", {}).get("get_star_ts", 0xFFFFFFFF)))
+            part_2_time = day_data.get("2", {}).get("get_star_ts")
             time_to_complete = None
             if part_2_time:
                 part_2_time = datetime.fromtimestamp(int(part_2_time))
